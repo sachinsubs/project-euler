be0227a6f6304bd42be3095d430683114991c7c2
Sachin Subramanian <sachin.subs@outlook.com>
2020-09-07 01:36:35 -0500

    skeleton code, NOTE problem has not been solved

---
+++ problems/pe0003.py
@@ -0,0 +1,26 @@
+# Project Euler Problem 0003
+# Largest prime factor
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+# NOTES:
+# - https://en.wikipedia.org/wiki/Formula_for_primes
+
+# Answer:
+
+# initialize variables
+# x1 = 1
+# x2 = 1
+# x_temp = 0
+
+# y = 0
+
+# while x2 < 4000000:
+#     x_temp = x1 + x2
+#     if x_temp % 2 == 0:
+#         y += x_temp
+#     x1 = x2
+#     x2 = x_temp
+
+# print(y)
